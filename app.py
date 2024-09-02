066523ca3b5a39836818be9b270ec6c74576f134
ranveerrajpal <ranveerrajpal19@gmail.com>
2024-09-02 14:44:24 +0530

    gunicorn

--- app.py
+++ app.py
@@ -8,6 +8,21 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 
+workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
+
+threads = int(os.environ.get('GUNICORN_THREADS', '4'))
+
+# timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
+
+bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
+
+
+
+forwarded_allow_ips = '*'
+
+secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+
+
 app = Flask(__name__)
 
 @app.route('/')
